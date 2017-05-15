e2c81597d7960c21224236f3dbc5a13dacb90c22
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-05-14 22:14:24 -1000

    passed brownOnly

--- arrayMethods.js
+++ arrayMethods.js
@@ -36,7 +36,7 @@ mixedWords.sort();
 var notFruit = fruitCollection.splice(1,3);
 gemBox.splice(5, 7, "Sapphire");
 upToTen.splice(2, 0, 3, 4, 5, 6, 7, 8);
-
+brownOnly.splice(1, 5, "brown", "brown", "brown");
 
 
 
